926863fecb6c4a39c616cfe9667b4d834253699f
Simon1197 <92693711+Simon1197@users.noreply.github.com>
2023-10-26 16:18:10 -0400

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,6 +1,5 @@
-CREATE OR REPLACE TABLE `data-with-darshil.uber_data_engineering_yt.tbl_analytics` AS (
-SELECT 
-f.trip_id
+Create or replace table `uber-project-403216.uber_data_engineering_py.tbl_analyics` AS (
+SELECT
 f.VendorID,
 d.tpep_pickup_datetime,
 d.tpep_dropoff_datetime,
@@ -16,17 +15,17 @@ f.fare_amount,
 f.extra,
 f.mta_tax,
 f.tip_amount,
-f.tolls_amount,
+f.total_amount,
 f.improvement_surcharge,
-f.total_amount
-FROM 
 
-`data-with-darshil.uber_data_engineering_yt.fact_table` f
-JOIN `data-with-darshil.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `data-with-darshil.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+FROM
+
+`uber_data_engineering_py.fact_table` f
+JOIN `uber_data_engineering_py.datetime_dim` d ON f.datetime_id = d.datetime_id
+JOIN `uber_data_engineering_py.passenger_count_dim` p ON p.passenger_count_id = f.passenger_count_id
+JOIN `uber_data_engineering_py.trip_distance_dim` t ON t.trip_distance_id = f.trip_distance_id
+JOIN `uber_data_engineering_py.rate_code_dim` r ON r.rate_code_id = f.rate_code_id
+JOIN `uber_data_engineering_py.pickup_location_dim` pick ON pick.pickup_location_id = f.pickup_location_id
+JOIN `uber_data_engineering_py.drop_location_dim` drop ON drop.dropoff_location_id = f.dropoff_location_id
+JOIN `uber_data_engineering_py.payment_type_dim` pay ON pay.payment_type_id = f.payment_type_id)
 ;
